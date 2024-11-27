cae31a22e1c18418201e3d5c18c1d20742d14258
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-11-27 13:16:33 -0500

    chore: removing some test stuff

--- src/events/ready.js
+++ src/events/ready.js
@@ -10,7 +10,4 @@ module.exports = async (Client) => {
     console.log(`Logged in as ${Client.user.tag}!`);
 
     const DB = await require('../handler/database.js')();
-
-    // const file = new Discord.AttachmentBuilder('https://thehungrybeast.com/wp-content/uploads/2022/08/2valorant-szachy_kv.jpg');
-    // await Client.guilds.cache.get('639477525927690240').channels.cache.get('898050443446464532').send({ files: [file] });
 }
